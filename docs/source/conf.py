a4c1a6793354ffa601e48442b187c620252bfb69
ネオン様 <122681564+Eineon@users.noreply.github.com>
2024-11-13 17:52:35 +0800

    up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,15 +3,14 @@
 # -- Project information
 
 project = '无色物语'
+html_title = '无色物语'
+
 copyright = '2024, ネオン様'
 author = 'Eineon'
-release = '~ End of The Shining World ~'
+
 version = '1.0'
 
-html_last_updated_fmt = "%Y/%m/%d"
-
 # -- General configuration
-
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -32,11 +31,18 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-html_theme = 'sphinx_material'
+html_static_path = ['_end']
+html_css_files = ['end-style.css']
+html_js_files = ['end-script.js']
 
-html_static_path = ["_end"]
-html_css_files = ["end-style.css"]
-html_js_files = ["end-script.js"]
+# -- HTML theme settings
+html_show_sourcelink = True
+html_last_updated_fmt = '%Y/%m/%d'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/Eineon/PalEnd",
+    "use_repository_button": True,
+}
 
 # -- Options for HTMLHelp output
 htmlhelp_basename = '无色物语'
